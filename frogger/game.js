c44e7e1d4af961f399eb665a3a0e93c414d6e222
bfisch02 <brett.fischler@tufts.edu>
2013-02-12 12:07:27 -0500

    Deleted var

--- frogger/game.js
+++ frogger/game.js
@@ -1,7 +1,8 @@
 function start_game() {
-	var my_level = 1;  
-	var my_score = 0;    
-	var high_score = 0; 
+	my_level = 1;
+	my_score = 0;    
+	high_score = 0;
+	remaining_lives = 3; 
         
         
         
@@ -19,19 +20,20 @@ function start_game() {
                 ctx.fillStyle = '#191970';
                 ctx.fillRect(0,0,399,290);
                 ctx.fillStyle = '#000000';
-                ctx.fillRect(0,275,399,300);
+                ctx.fillRect(0,280,399,300);
                 img = new Image();
                 img.src = 'assets/frogger_sprites.png';
                 ctx.drawImage(img, 0, 0, 399, 113, 0, 0, 399, 113);
-                ctx.drawImage(img, 0, 116, 399, 36, 0, 267, 399, 36);
+                ctx.drawImage(img, 0, 116, 399, 36, 0, 275, 399, 36);
                 ctx.drawImage(img, 0, 116, 399, 36, 0, 490, 399, 36);
-                
+                ctx.drawImage(img, 8, 326, 24, 27, 0, 528, 15, 16.875);
+                if (remaining_lives > 1)
+                	ctx.drawImage(img, 8, 326, 24, 27, 12, 528, 15, 16.875);
+                if (remaining_lives > 2)
+                	ctx.drawImage(img, 8, 326, 24, 27, 24, 528, 15, 16.875);
                 
            
-       /* width = image.width;
-        height = image.height;
-        canvas = $("#canvas")[0];
-        context = canvas.getContext("2d");
+  
     /*   
                 setInterval(start_game) () {
             context.clearRect(0, 0, canvas.width, canvas.height);
@@ -75,7 +77,7 @@ function start_game() {
                 // Draw some text
                 ctx.fillStyle="rgb(0, 255, 0)";
                 ctx.font = "bold 20px arial";
-                ctx.fillText("Level " + my_level, 50, 546);
+                ctx.fillText("Level " + my_level, 47, 546);
                 ctx.font = "bold 12px arial";
                 ctx.fillText("Score: " + my_score, 0, 562);
                 ctx.fillText("High Score: " + high_score, 90, 562);
