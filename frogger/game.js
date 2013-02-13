76a02cb93f89b7b21ce498e080d94e3ff3c86997
bfisch02 <brett.fischler@tufts.edu>
2013-02-12 23:44:42 -0500

    Added sprites for assignment 1

--- frogger/game.js
+++ frogger/game.js
@@ -3,87 +3,58 @@ function start_game() {
 	my_score = 0;    
 	high_score = 0;
 	remaining_lives = 3; 
-        
-        
-        
-            canvas = document.getElementById('game');
-            
-            // Check if canvas is supported on browser
-            if (canvas.getContext) {
-            
-                ctx = canvas.getContext('2d');
-                
-                // Save the default settings (e.g., transformations) now
-                ctx.save();
-                
-                
-                ctx.fillStyle = '#191970';
-                ctx.fillRect(0,0,399,290);
-                ctx.fillStyle = '#000000';
-                ctx.fillRect(0,280,399,300);
-                img = new Image();
-                img.src = 'assets/frogger_sprites.png';
-                ctx.drawImage(img, 0, 0, 399, 113, 0, 0, 399, 113);
-                ctx.drawImage(img, 0, 116, 399, 36, 0, 275, 399, 36);
-                ctx.drawImage(img, 0, 116, 399, 36, 0, 490, 399, 36);
-                ctx.drawImage(img, 8, 326, 24, 27, 0, 528, 15, 16.875);
-                if (remaining_lives > 1)
-                	ctx.drawImage(img, 8, 326, 24, 27, 12, 528, 15, 16.875);
-                if (remaining_lives > 2)
-                	ctx.drawImage(img, 8, 326, 24, 27, 24, 528, 15, 16.875);
-                
-           
+    timer = 0;       
+    canvas = document.getElementById('game');
+    if (canvas.getContext) {
+    	ctx = canvas.getContext('2d');
+        ctx.save();
+          
+        start_animation();
+    }
+    
+    else {
+        alert('Sorry, canvas is not supported on your browser!');
+	}
+}
+
+function start_animation()
+{
+	delay = 40; // milliseconds
+    setInterval(draw_image, delay); // draw refers to the function  
+
+}     
   
-    /*   
-                setInterval(start_game) () {
-            context.clearRect(0, 0, canvas.width, canvas.height);
-            context.drawImage(image, xpos, ypos, frameSize, frameSize, 0, 0, frameSize, frameSize);
-            xpos += frameSize;
-            index += 1;
-            if(index >= numFrames) {
-                xpos = 0;
-                ypos = 0;
-                index = 0;
+function draw_image()
+{
+	timer ++;
+	ctx.clearRect(0,0,canvas.width,canvas.height);
+ 		ctx.fillStyle = '#191970';
+        ctx.fillRect(0,0,399,290); 
+        ctx.fillStyle = '#000000';
+        ctx.fillRect(0,280,399,300);
+	img = new Image();
+    img.src = 'assets/frogger_sprites.png';
+    ctx.drawImage(img, 0, 0, 399, 113, 0, 0, 399, 113);
+    ctx.drawImage(img, 0, 116, 399, 36, 0, 275, 399, 36);
+    ctx.drawImage(img, 0, 116, 399, 36, 0, 490, 399, 36);
+    ctx.drawImage(img, 8, 326, 24, 27, 0, 528, 15, 16.875);
+    if (remaining_lives > 1)
+        ctx.drawImage(img, 8, 326, 24, 27, 12, 528, 15, 16.875);
+    if (remaining_lives > 2)
+        ctx.drawImage(img, 8, 326, 24, 27, 24, 528, 15, 16.875);
     
-            }
-            else if(xpos + frameSize > width) {
-                xpos = 0;
-                ypos += frameSize;
-            }
-        }, 1000/24);
-   */             
-                
-                
-                // Draw rects
-             
-           /*     ctx.fillStyle = "#191970";
-                ctx.fillRect(50, 50, 100, 75);
-                ctx.strokeRect(25, 25, 150, 125);
-                ctx.clearRect(75, 75, 25, 25);
-  */              
-                // Draw circle
-/*                ctx.arc(300, 300, 50, 0, 260, true);
-                  ctx.fill();
- */               
-                // Draw line
- /*               ctx.strokeStyle = '#FF0000';
-                ctx.lineWidth = 10;
-                ctx.lineCap = 'round';
-                ctx.beginPath();
-                ctx.moveTo(10, 400);
-                ctx.lineTo(400, 450);
-               ctx.stroke();
-  */              
-                // Draw some text
-                ctx.fillStyle="rgb(0, 255, 0)";
-                ctx.font = "bold 20px arial";
-                ctx.fillText("Level " + my_level, 47, 546);
-                ctx.font = "bold 12px arial";
-                ctx.fillText("Score: " + my_score, 0, 562);
-                ctx.fillText("High Score: " + high_score, 90, 562);
-            }
-            else {
-                alert('Sorry, canvas is not supported on your browser!');
-            }
-        }
+    ctx.drawImage(img, 10, 366, 27, 22, 175, 498, 27, 22);
+    ctx.drawImage(img, 5, 226, 90, 30, 0, 210, 90, 30);
+    ctx.drawImage(img, 80, 262, 31, 29, 175, 458, 31, 29);
+    ctx.drawImage(img, 44, 262, 31, 29, 175, 350, 31, 29);
+    // ctx.drawImage(img, 5, 226, 90, 30, 0, 240, 90, 30);
     
+    ctx.fillStyle="rgb(0, 255, 0)";
+    ctx.font = "bold 20px arial";
+    ctx.fillText("Level " + my_level, 47, 546);
+    ctx.font = "bold 12px arial";
+    ctx.fillText("Score: " + my_score, 0, 562);
+    ctx.fillText("High Score: " + high_score, 90, 562);
+                	
+}
+
