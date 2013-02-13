d29d9f5ef03d90d0eea166b4cc17e1824ec90e92
bfisch02 <brett.fischler@tufts.edu>
2013-02-12 20:24:50 -0500

    Final Ms Pacman Style

--- mspacman/mspacman.js
+++ mspacman/mspacman.js
@@ -8,9 +8,9 @@ function start_game()
 		img = new Image();
 		
             img.src = 'pacman10-hp-sprite.png';
-            ctx.drawImage(img, 322, 0, 464, 138, 0, 0, 464, 138);
-            ctx.drawImage(img, 81, 22, 17, 17, 280, 117, 17, 17);
-            ctx.drawImage(img, 82, 82, 17, 17, 280, 54, 17, 17);
+            ctx.drawImage(img, 322, 2, 464, 138, 0, 0, 464, 136);
+            ctx.drawImage(img, 81, 22, 17, 17, 280, 113, 17, 17);
+            ctx.drawImage(img, 82, 82, 17, 17, 280, 50, 17, 17);
        //     ctx.drawImage(img, 8, 326, 24, 27, 0, 528, 15, 16.875);
 	
 	
